e8368c21a7f92de05554c7f862c93e1a2e92fee7
molinii <108532374+molinii@users.noreply.github.com>
2024-11-29 09:04:41 -0300

    Add files via upload

---
+++ Redes Sociais Pedro Molini/quantidadeUsuarios.js
@@ -0,0 +1,151 @@
+// código omitido
+
+const layout = {
+    plot_bgcolor: getCSS('--bg-color'),
+    paper_bgcolor: getCSS('--bg-color'),
+    title: {
+      text: 'Redes sociais com mais usuários no mundo'
+    }
+  }
+  
+  // código omitido
+  // código omitido
+
+const layout = {
+    plot_bgcolor: getCSS('--bg-color'),
+    paper_bgcolor: getCSS('--bg-color'),
+    title: {
+      text: 'Redes sociais com mais usuários no mundo',
+       x: 0
+    }
+  }
+  
+  // código omitido
+  // código omitido
+
+const layout = {
+    plot_bgcolor: getCSS('--bg-color'),
+    paper_bgcolor: getCSS('--bg-color'),
+    title: {
+      text: 'Redes sociais com mais usuários no mundo',
+       x: 0,
+       font: {
+           color: getCSS('--primary-color')
+       }
+    }
+  }
+  
+  // código omitido
+  // código omitido
+
+const layout = {
+    plot_bgcolor: getCSS('--bg-color'),
+    paper_bgcolor: getCSS('--bg-color'),
+    title: {
+      text: 'Redes sociais com mais usuários no mundo',
+       x: 0,
+       font: {
+           color: getCSS('--primary-color'),
+           family: getCSS('--font'),
+           size: 30
+       }
+    }
+  }
+  
+  // código omitido
+  /* código omitido */
+
+.grafico {
+    margin-top: 3rem;
+}
+
+/* código omitido */
+// código omitido
+
+const layout = {
+    plot_bgcolor: getCSS('--bg-color'),
+    paper_bgcolor: getCSS('--bg-color'),
+    title: {
+      text: 'Redes sociais com mais usuários no mundo',
+       x: 0,
+       font: {
+           color: getCSS('--primary-color'),
+           family: getCSS('--font'),
+           size: 30
+       }
+    },
+      xaxis: {
+      
+      },
+      yaxis: {
+      
+      }
+  }
+  
+  // código omitido
+  // código omitido
+
+},
+xaxis: {
+    title: {
+        text: 'nome das redes sociais',
+        font: {
+            color: getCSS('--secondary-color')
+        }
+    }
+},
+yaxis: {
+
+}
+}
+
+// código omitido
+// código omitido
+
+},
+xaxis: {
+    title: {
+        text: 'nome das redes sociais',
+        font: {
+            color: getCSS('--secondary-color')
+        }
+    }
+},
+yaxis: {
+    title: {
+        text: 'bilhões de usuários ativos',
+        font: {
+            color: getCSS('--secondary-color')
+        }
+    }
+}
+}
+
+// código omitido
+import { getCSS, tickConfig } from "./common.js"
+
+
+// código omitido
+// código omitido
+
+},
+xaxis: {
+    tickfont: tickConfig,
+    title: {
+        text: 'nome das redes sociais',
+        font: {
+            color: getCSS('--secondary-color')
+        }
+    }
+},
+yaxis: {
+    tickfont: tickConfig,
+    title: {
+        text: 'bilhões de usuários ativos',
+        font: {
+            color: getCSS('--secondary-color')
+        }
+    }
+}
+
+// código omitido
